fa9be9f2469f0381af07a8be5f5c56108b9a0d2f
Jdevvv <llanta.jeremy@gmail.com>
2020-05-08 19:33:44 +0200

    leftnav

--- resources/views/includes/leftnav.blade.php
+++ resources/views/includes/leftnav.blade.php
@@ -1,1 +1,27 @@
-<!-- TODO -->
+<nav class="col-md-2 d-none d-md-block bg-light sidebar position-absolute mt-4">
+    <div class="sidebar-sticky">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link" href="/home">
+                    Accueil
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    Créer ma recette
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    Liste des ingrédients
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    Mes recettes
+            </a>
+            </li>
+
+        </ul>
+    </div>
+</nav>
