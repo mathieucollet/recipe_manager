8154b5b1e1c1b55734d7499d7ad0b65ece91a5d5
Jdevvv <llanta.jeremy@gmail.com>
2020-05-08 19:09:44 +0200

    topnav

--- resources/views/includes/topnav.blade.php
+++ resources/views/includes/topnav.blade.php
@@ -1,1 +1,4 @@
-<!-- TODO -->
+<nav class="navbar navbar-light bg-dark ">
+    <a href="/home" class="navbar-brand text-white font-weight-bold">My Super Recipe Manager</a>
+    <div class="text-white">Nom de l'utilisateur</div>
+</nav>
