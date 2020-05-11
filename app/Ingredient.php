4ebd56866c81b50445a76b391776eea3a9d46a51
Mayeut <mayeut@gmail.com>
2020-05-11 13:19:53 +0200

    @fin_ingredients

--- app/Ingredient.php
+++ app/Ingredient.php
@@ -5,6 +5,10 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property mixed id
+ * @property mixed user
+ */
 class Ingredient extends Model
 {
     protected $guarded = [];
@@ -15,5 +19,18 @@ class Ingredient extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return string
+     */
+    public function path()
+    {
+        return "/ingredients/{$this->id}";
+    }
+
+    public function home()
+    {
+        return "/ingredients";
     }
 }
