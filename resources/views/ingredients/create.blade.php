98e12efa323c02fdc035a3486507a30a2061ac06
Cédric B <cedricbardaine@gmail.com>
2020-05-11 22:13:24 +0200

    create ingredient page done

--- resources/views/ingredients/create.blade.php
+++ resources/views/ingredients/create.blade.php
@@ -1,3 +1,32 @@
-<form action="/ingredients" method="post">
+@extends('layouts.app')
+
+@section('content')
+
+<form action="/ingredient" method="post">
     @csrf
+    
+    <div class="col-md-9 ml-sm-auto col-lg-10">
+        
+        <div class="form-group">
+            <label for="in_nom">Nom : </label>
+            <input type="text" class="form-control" id="in_nom" name="name" placeholder="Tomate">
+            @error('name')
+            <small class="form-text text-danger">{{$message}}</small>
+            @enderror
+        </div>
+        
+        <div class="form-group">
+            <label for="in_prix">Prix : </label>
+            <input type="text" class="form-control" id="in_prix" name="price" placeholder="1.50">
+            @error('price')
+            <small class="form-text text-danger">{{$message}}</small>
+            @enderror
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Ajouter mon nouvel ingrédent !</button>
+        
+    </div>
 </form>
+
+
+@endsection
