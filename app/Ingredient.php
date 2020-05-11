83f15a0bf4c1a6f4f4a5870838e010903549c89a
Mayeut <mayeut@gmail.com>
2020-05-11 12:53:37 +0200

    @start_ingredients

--- app/Ingredient.php
+++ app/Ingredient.php
@@ -3,8 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Ingredient extends Model
 {
-    //
+    protected $guarded = [];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/factories/IngredientFactory.php
+++ database/factories/IngredientFactory.php
@@ -5,8 +5,12 @@
 use App\Ingredient;
 use Faker\Generator as Faker;
 
-$factory->define(Ingredient::class, function (Faker $faker) {
-    return [
-        //
-    ];
-});
+$factory->define(
+    Ingredient::class,
+    function (Faker $faker) {
+        return [
+            'name'  => $faker->name,
+            'price' => $faker->randomFloat(2, 0, 15),
+        ];
+    }
+);
