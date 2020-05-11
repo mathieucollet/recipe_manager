adad9db25ee4338d899d10208f99cf6051e55613
Cédric B <cedricbardaine@gmail.com>
2020-05-11 22:43:50 +0200

    create edit_ingredient_page ; in page ingredients_index : add links to show page for each ingredient & a button to modify (redirect to edit page)

--- resources/views/ingredients/edit.blade.php
+++ resources/views/ingredients/edit.blade.php
@@ -1,3 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+
 <form action="{{$ingredient->path()}}" method="post">
-    @method('patch')
+@csrf
+@method('patch')
+
+<div class="row">
+    
+    <div class="col-10">
+        <div class="col-md-9 ml-sm-auto col-lg-10">
+            <div class="form-group">
+            <label for="in_nom">Nouveau nom : </label>
+            <input type="text" class="form-control" id="in_nom" name="name" placeholder="Ancien nom : {{$ingredient->name}}">
+            @error('name')
+            <small class="form-text text-danger">{{$message}}</small>
+            @enderror
+        </div>
+        
+        <div class="form-group">
+            <label for="in_prix">Nouveau prix : </label>
+            <input type="text" class="form-control" id="in_prix" name="price" placeholder="Ancien prix : {{$ingredient->price}}">
+            @error('price')
+            <small class="form-text text-danger">{{$message}}</small>
+            @enderror
+        </div>
+
+        <button type="submit" class="btn btn-primary">Modifier l'ingrédient !</button>
+
+        </div>
+    </div>
+</div>
+
 </form>
+
+@endsection
--- resources/views/ingredients/index.blade.php
+++ resources/views/ingredients/index.blade.php
@@ -3,40 +3,46 @@
 
 <div class="container-fluid">
     <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-9 ml-sm-auto col-lg-10">
-                    
-                    <form action="/ingredient/create">
-                        <button type="submit" class="btn btn-primary">Ajouter mon nouvel ingrédient</button>
-                    </form>
-                    <br>
-                    
-                    <!-- Version left-align simple -->
-                    <!-- <ul class="list-group">
-                        @foreach ($ingredients as $ingredient)
-                        <li class="list-group-item"> <b> {{ $ingredient->name }} </b> : {{ $ingredient->price }}€</li>
-                        @endforeach
-                    </ul> -->
-                    
-                    <ul class="list-group">
-                        @foreach ($ingredients as $ingredient)
-                        <li class="list-group-item">
-
-                            <div class="row">
-                                <div class="col" style="text-align: right;">
-                                    <b> {{ $ingredient->name }} </b> : 
-                                </div>
-                                <div class="col">
-                                    {{ $ingredient->price }}€
-                                </div>
+        <div class="row">
+            <div class="col-md-9 ml-sm-auto col-lg-10">
+                
+                <form action="/ingredient/create">
+                    <button type="submit" class="btn btn-primary">Ajouter mon nouvel ingrédient</button>
+                </form>
+                <br>
+                
+                <!-- Version left-align simple -->
+                <!-- <ul class="list-group">
+                    @foreach ($ingredients as $ingredient)
+                    <li class="list-group-item"> <b> {{ $ingredient->name }} </b> : {{ $ingredient->price }}€</li>
+                    @endforeach
+                </ul> -->
+                
+                <ul class="list-group">
+                    @foreach ($ingredients as $ingredient)
+                    <li class="list-group-item">
+                        
+                        <div class="row">
+                            <div class="col" style="text-align: right;">
+                                <b> <a href="/ingredient/{{ $ingredient->id }}" style="text-decoration: none; color: black;">{{ $ingredient->name }}</a> :  </b> 
+                                 
                             </div>
-
-                        </li>
-                        @endforeach
-                    </ul>
-
-                </div>
+                            <div class="col">
+                                {{ $ingredient->price }}€
+                            </div>
+                            <div class="col" style="max-width: fit-content;">
+                                <form action="/ingredient/{{ $ingredient->id }}/edit">
+                                    <button type="submit" class="btn btn-primary">Modifier l'ingrédient</button>
+                                </form>
+                            </div>
+                        </div>
+                        
+                    </li>
+                    @endforeach
+                </ul>
+                
             </div>
+        </div>
     </div>
 </div>
 
