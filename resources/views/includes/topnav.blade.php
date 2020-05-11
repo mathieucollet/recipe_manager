637ed8e65e51d3d92902861f5f7ef19492296aa0
Jdevvv <llanta.jeremy@gmail.com>
2020-05-11 19:02:09 +0200

    azerty

--- resources/views/includes/topnav.blade.php
+++ resources/views/includes/topnav.blade.php
@@ -1,4 +1,4 @@
 <nav class="navbar navbar-light bg-dark fixed-top">
     <a href="{{ route('home') }}" class="navbar-brand text-white font-weight-bold">My Super Recipe Manager</a>
-    <div class="text-white">{{ auth()->user()->name }}</div>
+    <div class="text-white">{{ auth()->user()->name ?? '' }}</div>
 </nav>
