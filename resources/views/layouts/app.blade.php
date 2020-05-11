7efc75238e019f928c95aedbc9439e55f857a0a7
Mayeut <mayeut@gmail.com>
2020-05-11 12:01:35 +0200

    select

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,8 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://unpkg.com/@popperjs/core@2/dist/umd/popper.js" defer></script>
+    <script src="{{asset('js/bootstrap-select.js')}}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
